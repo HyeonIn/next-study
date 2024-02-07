83aedc11be28fe7a105d1448ade412f65635f0c5
HyeonIn <gusdlsdlek@gmail.com>
2024-02-07 16:25:57 +0900

    NEXT project initialize

---
+++ next-app/app/page.tsx
@@ -0,0 +1,16 @@
+const page = () => {
+  return (
+    <div>
+      <p
+        style={{
+          fontSize: "24px",
+        }}
+      >
+        Next 공부 시작
+      </p>
+      <p>안녕하세요! 최현인입니다.</p>
+    </div>
+  );
+};
+
+export default page;
